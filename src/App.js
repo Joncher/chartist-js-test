66505a3f0569d7d98ef35a92d4675f0678e83062
John Oncher <johnoncher@gmail.com>
2019-07-23 09:39:32 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Bar from "./components/Bar";
+import Pie from "./components/Pie";
+import Line from "./components/Line";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = { activeItem: "bio" };
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/bar">Bar</Link>
+              </li>
+              <li>
+                <Link to="/pie/">Pie</Link>
+              </li>
+              <li>
+                <Link to="/line/">Line</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/bar" component={Bar} />
+          <Route path="/pie/" component={Pie} />
+          <Route path="/line/" component={Line} />
+        </div>
+      </Router>
+    );
+  }
 }
-
-export default App;
